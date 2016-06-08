0c7964689a9c097c692abb6a9852aca2341f4f89
Stephen Kempisty <stephen.kempisty@gmail.com>
2016-06-08 12:27:28 -0700

    add edit and update post functionality to controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,9 +24,17 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.save
+      @post.update(post_params)
+      redirect_to posts_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
