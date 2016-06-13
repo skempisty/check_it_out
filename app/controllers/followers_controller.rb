27b54566410fa18ad3fc8db2c9a04c42c5a50607
Stephen Kempisty <stephen.kempisty@gmail.com>
2016-06-13 14:31:49 -0700

    following and unfollowing now redirects_to :back

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -3,12 +3,12 @@ class FollowersController < ApplicationController
   def create
     @followed = User.find(params[:id])
     @followed.followers << current_user
-    redirect_to posts_path
+    redirect_to :back
   end
 
   def destroy
     followedId = params[:id]
     Following.find_by(user_id: followedId, follower_id: current_user.id).destroy
-    redirect_to posts_path
+    redirect_to :back
   end
 end
